b5fbb34de36b766da86cc8598d4a455fe9ced8a3
Andrew MacKie-Mason <drewmm@gmail.com>
2013-03-21 21:52:03 -0500

    SPACES NOT TABS DAMMIT

--- AMMNCWundergroundController-new.h
+++ AMMNCWundergroundController-new.h
@@ -8,18 +8,19 @@
 static NSBundle *_ammNCWundergroundWeeAppBundle = nil;
 
 @interface ammNCWundergroundController: NSObject <BBWeeAppController> {
-	// view and model
-	AMMNCWundergroundView *i_view;
-	AMMNCWundergroundModel *i_model;
+    // view and model
+    AMMNCWundergroundView *i_view;
+    AMMNCWundergroundModel *i_model;
 
-	// other things
-	NSString *i_saveFile;
-	CLLocationManager *i_locationManager;
-	BOOL i_locationUpdated; // prevent location from updating more than once per refresh
-	BOOL i_loadingData; // prevent reloading data while already doing so
-	float i_baseWidth; // width our views are based on (min width it should ever display at)
-	float i_currentWidth; // current width of the screen, orientation dependent
-	NSDictionary *i_iconMap;
+    // other things
+    NSString *i_saveFile;
+    CLLocationManager *i_locationManager;
+    BOOL i_locationUpdated; // prevent location from updating more than once per refresh
+    BOOL i_loadingData; // prevent reloading data while already doing so
+    float i_baseWidth; // width our views are based on (min width it should ever display at)
+    float i_currentWidth; // current width of the screen, orientation dependent
+    NSDictionary *i_iconMap;
+    float i_viewHeight;
 }
 
 @property (nonatomic, readonly) NSString *saveFile;
