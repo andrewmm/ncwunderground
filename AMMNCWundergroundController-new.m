525107e9f1737a51364b32412f65a9d7c6a36c6b
Andrew MacKie-Mason <drewmm@gmail.com>
2013-03-23 14:55:30 -0400

    Removed a TODO comment

--- AMMNCWundergroundController-new.m
+++ AMMNCWundergroundController-new.m
@@ -13,8 +13,7 @@
     _ammNCWundergroundWeeAppBundle = [[NSBundle bundleForClass:[self class]] retain];
 }
 
-- (id)init { // View should be created and destroyed in loadPlaceholderView and loadFullView/unloadView, not here
-             // model should be created and destroyed in init/dealloc
+- (id)init {
     if ((self = [super init]) != nil) {
         i_viewHeight = 71;
         i_baseWidth = [UIScreen mainScreen].bounds.size.width;
