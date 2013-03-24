3d0c3c23d9b9e70bcf8c3f3eff0a0ef7f47f39e3
Andrew MacKie-Mason <drewmm@gmail.com>
2013-03-24 19:02:05 -0400

    Lots of changes. Oops.

--- AMMNCWundergroundModel.m
+++ AMMNCWundergroundModel.m
@@ -149,4 +149,185 @@
         @"icon_url"] lastPathComponent] stringByDeletingPathExtension];
 }
 
+// Takes: index into daily forecast array
+// Returns: short name of the corresponding day (Mon, Tue, etc)
+- (NSString *)dailyDayShortString:(int)forecastIndex {
+    return [[[[i_saveData objectForKey:@"forecastday"] objectAtIndex:
+        forecastIndex] objectForKey:@"date"] objectForKey:@"weekday_short"];
+}
+
+// Takes: index into daily forecast array
+// Returns: high temperature for that day, NOT including °F
+- (NSString *)dailyHighStringF:(int)forecastIndex {
+    return [[[[i_saveData objectForKey:@"forecastday"] objectAtIndex:
+        forecastIndex] objectForKey:@"high"] objectForKey:@"fahrenheit"];
+}
+
+// Takes: index into daily forecast array
+// Returns: low temperature for that day, NOT including °F
+- (NSString *)dailyLowStringF:(int)forecastIndex {
+    return [[[[i_saveData objectForKey:@"forecastday"] objectAtIndex:
+        forecastIndex] objectForKey:@"low"] objectForKey:@"fahrenheit"];
+}
+
+// Takes: index into daily forecast array
+// Returns: percentage of perciptation for that day, including %
+- (NSString *)dailyPOPString:(int)forecastIndex {
+    return [NSString stringWithFormat:@"%@%%",[[[[i_saveData objectForKey:@"forecastday"] objectAtIndex:
+        forecastIndex] objectForKey:@"low"] stringValue]];
+}
+
+// Takes: index into daily forecast array
+// Returns: name of icon for weather for that day
+- (NSString *)dailyConditionsIconName:(int)forecastIndex {
+    return [[[[[i_saveData objectForKey:@"forecastday"] objectAtIndex:
+        forecastIndex] objectForKey:@"icon_url"] lastPathComponent] 
+            stringByDeletingPathExtension];
+}
+
+// Takes: path to the save file
+// Returns: YES if it was able to load data, NO otherwise
+- (BOOL)loadSaveData:(NSString *)saveFile {
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+
+    if ([fileManager fileExistsAtPath:i_saveFile]) {
+        NSMutableDictionary *tempDict = [[NSMutableDictionary alloc] initWithContentsOfFile:i_saveFile];
+        [i_savedData addEntriesFromDictionary:tempDict];
+        [tempDict release];
+        if ([i_savedData objectForKey:@"last_request"] == nil) {
+            NSLog(@"NCWunderground: save file exists, but appears corrupted.");
+            return NO;
+        }
+        return YES;
+    }
+    else {
+        return NO;
+    }
+}
+
+// This should only ever run inside the backgroundQueue
+- (void) startURLRequest {
+    // get data from website by HTTP GET request
+    NSHTTPURLResponse * response;
+    NSError * error;
+
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    NSDictionary *defaultsDom = [[NSUserDefaults standardUserDefaults] persistentDomainForName:@"com.amm.ncwunderground"];
+    NSString *apiKey = [defaultsDom objectForKey:@"APIKey"];
+    if (apiKey == nil) {
+        NSLog(@"NCWunderground: got null APIKey, not updating data.");
+        [request release];
+
+        // TODO
+        // We shouldn't silently fail here. We should overwrite the
+        // views with something instructing the user to enter the API key.
+
+        return;
+    }
+    NSString *urlString = [NSString stringWithFormat:
+        @"http://api.wunderground.com/api/%@/conditions/hourly/forecast10day/q/%@,%@.json",
+        apiKey,[i_savedData objectForKey:@"latitude"],
+        [i_savedData objectForKey:@"longitude"]];
+    [request setURL:[NSURL URLWithString:urlString]];
+    [request setHTTPMethod:@"GET"];
+
+    NSData *resultJSON = [NSURLConnection sendSynchronousRequest:
+        request returningResponse:&response error:&error];
+    [request release];
+    if (!resultJSON) {
+        NSLog(@"NCWunderground: Unsuccessful connection attempt. Data not updated.");
+        // TOOD: Add a red dot somewhere to indicate last update failed?
+        return;
+    }
+    else {
+        // TODO: clear the red dot?
+    }
+
+    if(NSClassFromString(@"NSJSONSerialization")) {
+        NSError *error = nil;
+        NSDictionary *jsonDict = [NSJSONSerialization JSONObjectWithData:resultJSON options:0 error:&error];
+        if (error) {
+            NSLog(@"NCWunderground: JSON was malformed. Bad.");
+            return;
+        }
+
+        if ([jsonDict objectForKey:@"error"]) {
+            NSLog(@"NCWunderground: We got a well-formed JSON, but it's an error: %@ / %@",
+                [[jsonDict objectForKey:@"error"] objectForKey:@"type"],
+                [[jsonDict objectForKey:@"error"] objectForKey:@"description"]);
+            return;
+        }
+
+        if ([jsonDict isKindOfClass:[NSDictionary class]]) {
+            NSLog(@"NCWunderground: Data download succeeded. Parsing.");
+            // update last-request time
+            [i_savedData setObject:[NSNumber numberWithInteger:
+                [[NSDate date] timeIntervalSince1970]] forKey:@"last_request"];
+
+            // convenience pointers
+            NSDictionary *currentObservation = [jsonDict objectForKey:
+                @"current_observation"];
+            NSDictionary *dailyForecast = [[jsonDict objectForKey:
+                @"forecast"] objectForKey:@"simpleforecast"];
+            NSDictionary *displayLocation = [currentObservation objectForKey:
+                @"display_location"];
+
+            // import current observations, daily forecast, and hourly forecast
+            [i_savedData setObject:currentObservation forKey:
+                @"current_observation"];
+            [i_savedData setObject:[dailyForecast objectForKey:
+                @"forecastday"] forKey:@"forecastday"];
+            [i_savedData setObject:[jsonDict objectForKey:
+                @"hourly_forecast"] forKey:@"hourly_forecast"];
+
+            // Tell the controller that the data has been updated
+            dispatch_async(dispatch_get_main_queue(),^(void) {
+                [i_controller dataDownloaded];
+            });
+        }
+        else {
+            NSLog(@"NCWunderground: JSON was non-dict. Bad.");
+            i_loadingData = NO;
+            return;
+        }
+    }
+    else {
+        NSLog(@"NCWunderground: We don't have NSJSONSerialization. Bad.");
+        i_loadingData = NO;
+        return;
+    }
+}
+
+// Does: Takes in updated location and sets it.
+//       Then starts the URL request on the background queue.
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    NSLog(@"NCWunderground: didUpdateToLocation: %@", [locations lastObject]);
+    if (i_locationUpdated == NO) {
+        if (locations != nil) {
+            [i_savedData setObject:[NSString stringWithFormat:
+                @"%.8f", [[locations lastObject] coordinate].latitude] forKey:
+                @"latitude"];
+            [i_savedData setObject:[NSString stringWithFormat:
+                @"%.8f", [[locations lastObject] coordinate].longitude] forKey:
+                @"longitude"];
+        
+            i_locationUpdated = YES;
+            [i_locationManager stopUpdatingLocation];
+
+            // start a URL request in the backgroundQueue
+            dispatch_async(backgroundQueue, ^(void) {
+                [self startURLRequest];
+            });
+        }
+        else {
+            NSLog(@"NCWunderground: didUpdateToLocation called but newLocation nil. Bad.");
+        }
+    }
+}
+
+
+// save data to disk for later use
+//[i_savedData writeToFile:i_saveFile atomically:YES];
+//NSLog(@"NCWunderground: data saved to disk at %@",i_saveFile);
+
 @end
