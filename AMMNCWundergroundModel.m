ea8bf03316d9d79972535b4ad1132f50a6c03246
Andrew MacKie-Mason <drewmm@gmail.com>
2013-03-25 11:42:05 -0400

    5 second timeout interval

--- AMMNCWundergroundModel.m
+++ AMMNCWundergroundModel.m
@@ -268,6 +268,7 @@
         [i_saveData objectForKey:@"longitude"]];
     [request setURL:[NSURL URLWithString:urlString]];
     [request setHTTPMethod:@"GET"];
+    [request setTimeoutInterval:5];
 
     NSData *resultJSON = [NSURLConnection sendSynchronousRequest:
         request returningResponse:&response error:&error];
