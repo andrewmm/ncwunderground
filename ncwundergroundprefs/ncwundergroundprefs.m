36174d94141cb18d100d64a7f921d49110328118
Andrew MacKie-Mason <drewmm@gmail.com>
2013-04-29 01:56:40 -0500

    Trying to get basic preferences stuff working; still crashes

--- ncwundergroundprefs/ncwundergroundprefs.m
+++ ncwundergroundprefs/ncwundergroundprefs.m
@@ -1,16 +1,22 @@
 #import <Preferences/Preferences.h>
 
-@interface ncwundergroundprefsListController: PSListController {
+@interface AMMNCWundergroundPrefsPrincipal: NSObject {
 }
 @end
 
-@implementation ncwundergroundprefsListController
-- (id)specifiers {
+@implementation AMMNCWundergroundPrefsPrincipal
+
+- (void)support {
+	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=E7PRS3GFR2VR2&lc=US&item_name=Andrew%20MacKie%2dMason&item_number=NCWunderground&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted"]];
+}
+/*- (id)specifiers {
 	if(_specifiers == nil) {
 		_specifiers = [[self loadSpecifiersFromPlistName:@"ncwundergroundprefs" target:self] retain];
 	}
 	return _specifiers;
 }
-@end
+@end*/
 
 // vim:ft=objc
+
+@end
