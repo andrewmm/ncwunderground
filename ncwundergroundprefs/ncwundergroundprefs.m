0e5c3599af4b40c3a6bcff2695c3537e71e7ff8e
Andrew MacKie-Mason <drewmm@gmail.com>
2013-05-09 11:19:41 -0500

    Whitespace

--- ncwundergroundprefs/ncwundergroundprefs.m
+++ ncwundergroundprefs/ncwundergroundprefs.m
@@ -12,14 +12,14 @@
 @implementation AMMNCWundergroundPrefsPrincipal
 
 - (void)support {
-	NSLog(@"Support pressed.");
-	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=E7PRS3GFR2VR2&lc=US&item_name=Andrew%20MacKie%2dMason&item_number=NCWunderground&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted"]];
+    NSLog(@"Support pressed.");
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=E7PRS3GFR2VR2&lc=US&item_name=Andrew%20MacKie%2dMason&item_number=NCWunderground&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted"]];
 }
 
 - (void)clearLocationPermissions {
-	NSFileManager *fileManager = [NSFileManager defaultManager];
-	NSString *path = @"/var/root/Library/Caches/locationd/clients.plist";
-	if ([fileManager fileExistsAtPath:path]) {
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSString *path = @"/var/root/Library/Caches/locationd/clients.plist";
+    if ([fileManager fileExistsAtPath:path]) {
         NSMutableDictionary *permissionsDictionary = [[NSMutableDictionary alloc] initWithContentsOfFile:path];
         [permissionsDictionary removeObjectForKey:@"com.apple.springboard"];
         [permissionsDictionary writeToFile:path atomically:YES];
@@ -27,7 +27,7 @@
         [NSTask launchedTaskWithLaunchPath:@"/usr/bin/killall" arguments:[NSArray arrayWithObjects:@"locationd",nil]];
     }
     else {
-    	NSLog(@"NCWunderground: Asked to clear location permissions, but %@ does not exist. Ignoring.",path);
+        NSLog(@"NCWunderground: Asked to clear location permissions, but %@ does not exist. Ignoring.",path);
     }
 }
 
