29366276640f46b56e3b4232b18baa7b8d0e27df
Andrew MacKie-Mason <drewmm@gmail.com>
2013-05-09 20:27:59 -0500

    Adding some new options on the About pane.

--- ncwundergroundprefs/ncwundergroundprefs.m
+++ ncwundergroundprefs/ncwundergroundprefs.m
@@ -4,16 +4,29 @@
 @interface AMMNCWundergroundPrefsPrincipal: NSObject {
 }
 
+- (void)github;
 - (void)support;
+- (void)license;
 - (void)clearLocationPermissions;
 
 @end
 
 @implementation AMMNCWundergroundPrefsPrincipal
 
+- (void)github {
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://github.com/andrewmm/ncwunderground"]];
+}
+
 - (void)support {
-    NSLog(@"Support pressed.");
     [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=E7PRS3GFR2VR2&lc=US&item_name=Andrew%20MacKie%2dMason&item_number=NCWunderground&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted"]];
+}
+
+- (void)license {
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://opensource.org/licenses/MIT"]];
+}
+
+- (void)email {
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"mailto://drewmm@gmail.com"]];
 }
 
 - (void)clearLocationPermissions {
@@ -23,10 +36,10 @@
         NSMutableDictionary *saveFile = [[NSMutableDictionary alloc] initWithContentsOfFile:path];
         [saveFile removeObjectForKey:@"locationPermissions"];
         [saveFile writeToFile:path atomically:YES];
-        NSLog(@"NCWunderground: Location permissions cleared.");
+        NSLog(@"NCWunderground Preferences: Location permissions cleared.");
     }
     else {
-        NSLog(@"NCWunderground: Asked to clear location permissions, but %@ does not exist.",path);
+        NSLog(@"NCWunderground Preferences: Asked to clear location permissions, but %@ does not exist.",path);
     }
 }
 
